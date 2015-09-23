1d8760ad2f3300f72487b802fb8006e2325d6c81
jonatan <jonh_gabriel05@hotmai.com>
2015-09-23 19:52:31 -0300

    modificaciones playlist con mapa

---
+++ www/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var width = $(".logo").css('width');
+    var top = $(".box").css('height');
+    $(".logo").css('height',width);
+    $(".logo").css('top',top/2);
+
+
+});
